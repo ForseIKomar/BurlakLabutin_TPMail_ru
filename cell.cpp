0a309638a18f097350eae966c547d17adbfaee53
ForseIKomar <33393253+ForseIKomar@users.noreply.github.com>
2017-12-21 00:48:02 +0300

    Add files via upload
    Антон, вот оно

---
+++ cell.cpp
@@ -0,0 +1,53 @@
+#include "cell.h"
+
+CCell::CCell( SPoint point )
+	: point_( point ), figure_( nullptr )
+{}
+
+
+void CCell::SetFigure( std::shared_ptr<CFigure> figure )
+{
+    figure_ = figure;
+}
+
+
+void CCell::SetBoard( std::shared_ptr<CBoard>& board )
+{
+    game_field_ = board;
+}
+
+
+std::shared_ptr<CBoard>& CCell::GetBoard()
+{
+	return game_field_;
+}
+    
+
+std::shared_ptr<CFigure>& CCell::GetFigure()
+{
+    return figure_;
+}
+
+    
+bool CCell::IsEmpty()
+{
+	return ( figure_ == nullptr ) ? true : false;
+}
+    
+    
+bool CCell::HasFigure()
+{
+	return ( figure_ != nullptr ) ? true : false;
+}
+
+
+short CCell::GetRowNumber()
+{
+	return point_.y;
+}
+
+
+short CCell::GetColumnNumber()
+{
+	return point_.x;
+}
---
+++ queen.hpp
@@ -0,0 +1,17 @@
+#ifndef __QUEEN_HPP__
+#define __QUEEN_HPP__
+
+#include <memory>
+#include <vector>
+
+#include "figure.hpp"
+
+class CQueen: public CFigure{
+public:
+CQueen( char queen_color );
+
+	void WhereCanMove(   std::vector<std::shared_ptr<CCell>> &potential_cells );
+	void WhereCanAttack( std::vector<std::shared_ptr<CCell>> &potential_cells );
+};
+
+#endif
